21af08d060b273b587f617c0049f342ee08d14a2
itsyourboihora <rabinchrestha95@gmail.com>
2026-07-14 17:33:20 +0545

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 # ---- YOUR SETTINGS ----
 BOT_TOKEN = "8693301178:AAF2nNn4Igy_Da5UW347uX7LLv95QfokVyc"
 OWNER_CHAT_ID = "7766147200"
-WELCOME_MESSAGE = "Special VC(with cowgirl pose, doggy pose, loud moan) spanking , tongue showing , playing : 3200k for 12 mins, 2200k for 8 mins
+WELCOME_MESSAGE = "Special VC spanking , tongue showing , playing : 3200k for 12 mins, 2200k for 8 mins
 Normal VC: 1700 for 10 mins, 1200k for 6 mins"
 # -------------------------
 
